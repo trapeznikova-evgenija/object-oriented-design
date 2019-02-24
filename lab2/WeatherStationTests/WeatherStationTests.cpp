8bebd7a4eb10455ed7e94306da325bc0db73bcf5
trapeznikova-evgenija <trapeznikiva.evg@yandex.ru>
2019-02-25 00:57:24 +0300

    Добавила тестирование нотификейшена по приоритетам

--- lab2/WeatherStationTests/WeatherStationTests.cpp
+++ lab2/WeatherStationTests/WeatherStationTests.cpp
@@ -1,24 +1,31 @@
 #include "stdafx.h"
 #include "../2.1/Observer.h"
 #include "../2.1/WeatherData.h"
+#include <vector>
+#include <string>
 
 using namespace std;
 
 class CNotifyTests: public IObserver<SWeatherInfo>
 {
 public:
-	CNotifyTests(CWeatherData& observable, int observerPriority)
-		: m_observable(observable)
+	CNotifyTests(CObservable<SWeatherInfo>& observable, int observerPriority, vector<int>& prioritiesSet)
+		: m_observable(observable),
+		m_observer_priority(observerPriority),
+		m_priorities_set(prioritiesSet)
+
 	{
 	}
 
 private:
-	void Update(const SWeatherInfo& data)
+	void Update(const SWeatherInfo& data) override
 	{
+		m_priorities_set.push_back(m_observer_priority);
 		m_observable.RemoveObserver(*this);
 	}
 
-	CWeatherData m_observable;
+	CObservable<SWeatherInfo>& m_observable;
+	vector<int> &m_priorities_set;
 	int m_observer_priority;
 };
 
@@ -33,12 +40,24 @@ TEST_CASE("Safely notify observers")
 TEST_CASE("Observer priority testing")
 {
 	CWeatherData weatherData;
+	vector<int> prioritiesSet;
 	
-	CNotifyTests testObserver1(weatherData, 1);
-	CNotifyTests testObserver2(weatherData, 2);
-	CNotifyTests testObserver4(weatherData, 5);
+	CNotifyTests testObserver1(weatherData, 1, prioritiesSet);
+	CNotifyTests testObserver2(weatherData, 2, prioritiesSet);
+	CNotifyTests testObserver3(weatherData, 5, prioritiesSet);
 	
+	weatherData.RegisterObserver(testObserver1, 1);
+	weatherData.RegisterObserver(testObserver2, 2);
+	weatherData.RegisterObserver(testObserver3, 5);
 
-	CHECK(true == true);
+	weatherData.NotifyObservers();
+
+	string resultPriorityString = "";
+	for (int i = 0; i < prioritiesSet.size(); i++)
+	{
+		resultPriorityString += to_string(prioritiesSet[i]);
+	}
+
+	CHECK(resultPriorityString == "125");
 
 };

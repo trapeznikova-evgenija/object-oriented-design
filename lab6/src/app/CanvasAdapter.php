da1eede41843a2777c08b0f81e58a3b1ba5cd428
trapeznikova-evgenija <trapeznikova.evg@yandex.ru>
2019-04-01 07:01:05 +0300

    lab6

---
+++ lab6/src/app/CanvasAdapter.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace app;
+
+
+class CanvasAdapter
+{
+    /* ModernGraphicsRenderer */
+    private $renderer;
+    /* ModernGraphicsRenderer Point */
+    private $currPoint;
+
+    public function moveTo(int $x, int $y)
+    {
+
+    }
+
+    public function lineTo(int $x, int $y)
+    {
+
+    }
+}
